bcf6a27ff5762d484efb73d17495bd0e618ab176
Ivo Jansch <ivo@egeniq.com>
2015-04-24 08:24:17 +0200

    Added simple HelloWorld.

---
+++ HelloWorld/osxtestcase/osxtestcase/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  osxtestcase
+//
+//  Created by Ivo Jansch on 24/04/15.
+//  Copyright (c) 2015 Buildozer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloWorld/osxtestcase/osxtestcase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  osxtestcase
+//
+//  Created by Ivo Jansch on 24/04/15.
+//  Copyright (c) 2015 Buildozer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
